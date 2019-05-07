5ac00cb4c1da1391514aafcb681de97f5b194239
Morshed <morshedalamustc22@gmail.com>
2019-05-08 01:05:32 +0600

    LayOut file added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-
+ import Layout from './componenets/Layout/Layout'
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Layout>
+          <p>Test App</p>
+        </Layout>
       </div>
     );
   }
 }
 
 export default App;
-  
+    
